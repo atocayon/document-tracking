4776e31066c53c44989f3656ee4bb3503eca7012
aljon <atocayon27@gmail.com>
2020-04-09 15:32:26 +0800

    fixed: account deletion

--- src/component/screens/userManagement/UserManagement.js
+++ src/component/screens/userManagement/UserManagement.js
@@ -10,12 +10,29 @@ import AddIcon from "@material-ui/icons/Add";
 import axios from "axios";
 import ListOfUsers from "./ListOfUsers";
 import Reactotron from "reactotron-react-js";
-import { withSnackbar } from 'notistack';
+import { withSnackbar } from "notistack";
+import Dialog from "@material-ui/core/Dialog";
+import DialogTitle from "@material-ui/core/DialogTitle";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogContentText from "@material-ui/core/DialogContentText";
+import DialogActions from "@material-ui/core/DialogActions";
+import { useTheme } from "@material-ui/core/styles";
+import useMediaQuery from "@material-ui/core/useMediaQuery";
+import Button from "@material-ui/core/Button";
+import DialogComponent from "./DialogComponent";
 function UserManagement(props) {
   const [sectionUsers, setSectionUsers] = useState([]);
   const [token, setToken] = useState("");
   const [userRole, setUserRole] = useState("");
   const [endSesion, setEndSession] = useState(false);
+  const [openDialog, setOpenDialog] = React.useState({
+    open: false,
+    status: "",
+    name: "",
+    id: null
+  });
+  const theme = useTheme();
+  const fullScreen = useMediaQuery(theme.breakpoints.down("sm"));
   useEffect(() => {
     const obj = getFromStorage("documentTracking");
 
@@ -47,16 +64,19 @@ function UserManagement(props) {
   }, []);
 
   const handleAccountRole = val => {
-    Reactotron.log(val);
     axios
-      .post("http://localhost:4000/dts/updateRole", { role: val.status, id: val.id })
+      .post("http://localhost:4000/dts/updateRole", {
+        role: val.status,
+        id: val.id
+      })
       .then(res => {
         const variant = "success";
-        props.enqueueSnackbar('User role updated...', {variant});
+        props.enqueueSnackbar(val.name + " role updated...", { variant });
         window.location.reload();
       })
       .catch(err => {
-        Reactotron.log(err);
+        const variant = "error";
+        props.enqueueSnackbar(err, { variant });
       });
   };
 
@@ -64,10 +84,49 @@ function UserManagement(props) {
 
   const handleAccountStatus = () => {};
 
-  const handleAccountDeletion = () => {};
+  const handleClose = () => {
+    setOpenDialog({
+      ...openDialog,
+      open: false
+    });
+  };
+
+  const handleAccountDeletion = val => {
+    setOpenDialog({
+      ...openDialog,
+      open: true,
+      status: val.status,
+      name: val.name,
+      id: val.id
+    });
+  };
+
+  const handleConfirmDeletion = () => {
+    axios
+      .post("http://localhost:4000/dts/updateStatus", {
+        status: openDialog.status,
+        id: openDialog.id
+      })
+      .then(res => {
+        props.enqueueSnackbar(openDialog.name + " deleted...");
+        window.location.reload();
+      })
+      .catch(err => {
+        const variant = "error";
+        props.enqueueSnackbar(err, { variant });
+      });
+  };
 
   return (
     <>
+      {openDialog && (
+        <DialogComponent
+          fullscreen={fullScreen}
+          openDialog={openDialog}
+          handleClose={handleClose}
+          handleConfirmDeletion={handleConfirmDeletion}
+        />
+      )}
       {endSesion && <Redirect to={"/"} />}
       <Paper
         elevation={3}
@@ -130,6 +189,7 @@ function UserManagement(props) {
             token={token}
             userRole={userRole}
             handleAccountRole={handleAccountRole}
+            handleAccountDeletion={handleAccountDeletion}
           />
         )}
       </Paper>
