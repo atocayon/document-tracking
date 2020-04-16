e533f6043852a7b2c78bc0e9871158cb071a5096
aljon <atocayon27@gmail.com>
2020-04-16 22:26:43 +0800

    temp: add documents

--- src/component/screens/newUserForm/Work.js
+++ src/component/screens/newUserForm/Work.js
@@ -1,27 +1,23 @@
 import React from "react";
 import InputField from "../../common/textField/InputField";
 import Select from "@material-ui/core/Select";
-
-export default function Work(props){
-
-    return(
-        <>
-            <div style={{marginLeft: 30,marginTop: 30, marginBottom: 30}}>
-                <div className={"row"}>
-                    <div className={"col-md-2"}></div>
-                    <div className={"col-md-8"}>
-                        <InputField
-                            id={"position"}
-                            name={"position"}
-                            label={"Work Position/Function"}
-                            variant={"outlined"}
-                            onChange={props.handleInput}
-                            error={props.error.position}
-                        />
-                    </div>
-                    <div className={"col-md-2"}></div>
-                </div>
-            </div>
-        </>
-    );
+import WorkIcon from '@material-ui/icons/Work';
+export default function Work(props) {
+  return (
+    <>
+        <br/>
+        <h5 style={{ textAlign: "left",color: "#2196F3" }}>
+           <WorkIcon/>&nbsp;Work Position
+        </h5>
+        <br/>
+      <InputField
+        id={"position"}
+        name={"position"}
+        label={"Work Position/Function"}
+        variant={"outlined"}
+        onChange={props.handleInput}
+        error={props.error.position}
+      />
+    </>
+  );
 }
