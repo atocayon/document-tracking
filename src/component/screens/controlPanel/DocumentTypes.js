119c0089d7e5dc80376435a461f2852b3148f746
aljon <atocayon27@gmail.com>
2020-05-29 10:23:27 +0800

    fixed: table pagination in control panel

--- src/component/screens/controlPanel/DocumentTypes.js
+++ src/component/screens/controlPanel/DocumentTypes.js
@@ -14,16 +14,16 @@ import TableBody from "@material-ui/core/TableBody";
 import TablePagination from "@material-ui/core/TablePagination";
 
 function DocumentTypes(props) {
-    const [page, setPage] = React.useState(0);
-    const [rowsPerPage, setRowsPerPage] = React.useState(5);
-    const handleChangePage = (event, newPage) => {
-        setPage(newPage);
-    };
+  const [page, setPage] = React.useState(0);
+  const [rowsPerPage, setRowsPerPage] = React.useState(5);
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
 
-    const handleChangeRowsPerPage = (event) => {
-        setRowsPerPage(parseInt(event.target.value, 10));
-        setPage(0);
-    };
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
+  };
   return (
     <div style={{ paddingBottom: 200, height: "100vh", overflow: "auto" }}>
       <AddNewDocumentType
@@ -53,52 +53,56 @@ function DocumentTypes(props) {
         </button>
       </div>
 
-        <TableContainer component={Paper}>
-            <Table>
-                <TableHead>
-                    <TableRow style={{ background: "#2196F3"}}>
-                        <TableCell style={{color: "#fff" }}>#</TableCell>
-                        <TableCell style={{color: "#fff" }}>Document Type</TableCell>
-                        <TableCell></TableCell>
-                    </TableRow>
-                </TableHead>
-                <TableBody>
-                    {props.documentTypes.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((document, index) => (
-                        <TableRow key={index}>
-                            <TableCell>{++index}</TableCell>
-                            <TableCell>{document.type}</TableCell>
-                            <TableCell>
-                                <button
-                                    className={"btn btn-sm "}
-                                    onClick={props.handleEditDocumentType.bind(null, document.id)}
-                                >
-                                    <EditIcon />
-                                </button>
-                                <button
-                                    className={"btn btn-sm "}
-                                    onClick={props.handleDeleteDocumentType.bind(null, {
-                                        id: document.id,
-                                        type: document.type
-                                    })}
-                                >
-                                    <DeleteOutlineIcon />
-                                </button>
-                            </TableCell>
-                        </TableRow>
-                    ))}
-                </TableBody>
-            </Table>
-            <TablePagination
-                rowsPerPageOptions={[5, 10, 20, 30, 50, 70, 80, 100]}
-                component="div"
-                count={props.documentTypes.length}
-                rowsPerPage={rowsPerPage}
-                page={page}
-                onChangePage={handleChangePage}
-                onChangeRowsPerPage={handleChangeRowsPerPage}
-            />
-        </TableContainer>
-
+      <TableContainer component={Paper}>
+        <Table>
+          <TableHead>
+            <TableRow style={{ background: "#2196F3" }}>
+              <TableCell style={{ color: "#fff" }}>#</TableCell>
+              <TableCell style={{ color: "#fff" }}>Document Type</TableCell>
+              <TableCell></TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {props.documentTypes
+              .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+              .map((document, index) => (
+                <TableRow key={index}>
+                  <TableCell>{++index}</TableCell>
+                  <TableCell>{document.type}</TableCell>
+                  <TableCell>
+                    <button
+                      className={"btn btn-sm "}
+                      onClick={props.handleEditDocumentType.bind(
+                        null,
+                        document.id
+                      )}
+                    >
+                      <EditIcon />
+                    </button>
+                    <button
+                      className={"btn btn-sm "}
+                      onClick={props.handleDeleteDocumentType.bind(null, {
+                        id: document.id,
+                        type: document.type,
+                      })}
+                    >
+                      <DeleteOutlineIcon />
+                    </button>
+                  </TableCell>
+                </TableRow>
+              ))}
+          </TableBody>
+        </Table>
+        <TablePagination
+          rowsPerPageOptions={[5, 10, 20, 30, 50, 70, 80, 100]}
+          component="div"
+          count={props.documentTypes.length}
+          rowsPerPage={rowsPerPage}
+          page={page}
+          onChangePage={handleChangePage}
+          onChangeRowsPerPage={handleChangeRowsPerPage}
+        />
+      </TableContainer>
     </div>
   );
 }
