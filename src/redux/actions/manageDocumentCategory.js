f93328ae15995cc619ddf5d6b5f0aed04c60e445
aljon <atocayon27@gmail.com>
2020-06-16 15:50:32 +0800

    fixed: add new document category

--- src/redux/actions/manageDocumentCategory.js
+++ src/redux/actions/manageDocumentCategory.js
@@ -3,22 +3,39 @@ import axios from "axios";
 import Reactotron from "reactotron-react-js";
 export function addNewDocCategory(token, category, socket) {
   return async function (dispatch) {
-      Reactotron.log(socket);
-    await socket.emit(
-      "addNewDocumentCategory",
-      token,
-      category,
-      (response) => {
-        if (response) {
-          if (response === "server error") {
-            alert("An error occurred");
-          }
+    Reactotron.log(socket);
+    await socket.emit("addNewDocumentCategory", token, category, (response) => {
+      if (response) {
+        if (response === "server error") {
+          alert("An error occurred");
+        }
 
-          if (response === "inserted") {
-            dispatch({type: actionTypes.ADD_NEW_DOCUMENT_CATEGORY, data: category});
-          }
+        if (response === "inserted") {
+          dispatch({
+            type: actionTypes.ADD_NEW_DOCUMENT_CATEGORY,
+            data: category,
+          });
         }
       }
-    );
+    });
+  };
+}
+
+export function fetchDocCategory(token, socket) {
+  return async function (dispatch) {
+    await socket.emit("fetchDocumentCategory", token, (response) => {
+      if (response) {
+        if (response === "server error") {
+          alert("An error occurred");
+        }
+      }
+    });
+
+    await socket.on("sectionDocCategory", (data) => {
+      dispatch({
+        type: actionTypes.FETCH_SECTION_DOCUMENT_CATEGORY,
+        data,
+      });
+    });
   };
 }
--- src/redux/reducers/manageDocumentCategory.js
+++ src/redux/reducers/manageDocumentCategory.js
@@ -2,7 +2,8 @@ import actionTypes from "../actions/actionTypes";
 
 export default function manageDocumentCategory(state = [], action) {
   switch (action.type) {
-
+    case actionTypes.FETCH_SECTION_DOCUMENT_CATEGORY:
+      return [...action.data];
     default:
       return state;
   }
