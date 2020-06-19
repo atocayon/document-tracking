108313180a4ae78b3c3bc4fb81941d3a7a5076bc
atocayon <actwhiteHat27@gmail.com>
2020-06-19 14:58:36 +0800

    db update

--- tools/documentTracking.sql
+++ tools/documentTracking.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Jun 16, 2020 at 05:23 AM
+-- Generation Time: Jun 19, 2020 at 08:58 AM
 -- Server version: 10.4.11-MariaDB
 -- PHP Version: 7.4.1
 
@@ -63,6 +63,44 @@ CREATE TABLE `documentLogs` (
   `date_time` datetime NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dumping data for table `documentLogs`
+--
+
+INSERT INTO `documentLogs` (`trans_id`, `document_id`, `user_id`, `remarks`, `destinationType`, `destination`, `status`, `notification`, `date_time`) VALUES
+(1, '1000000000-1', '58', 'none', 'Internal', 'GSAS', '2', '0', '2020-06-18 09:20:49'),
+(2, '1000000000-2', '58', 'none', 'Internal', 'LRS', '2', '0', '2020-06-18 09:20:49'),
+(3, '1000000000-3', '58', 'none', 'Internal', 'NMPMNL', '2', '0', '2020-06-18 09:20:49'),
+(4, '1000000001-1', '58', 'none', 'Internal', 'HRMS', '2', '0', '2020-06-18 09:55:34'),
+(5, '1000000001-2', '58', 'none', 'Internal', 'MTS', '2', '0', '2020-06-18 09:55:34'),
+(6, '1000000002-1', '58', 'none', 'Internal', 'OED', '2', '0', '2020-06-19 14:28:04'),
+(7, '1000000002-2', '58', 'none', 'Internal', 'IMS', '2', '0', '2020-06-19 14:28:04'),
+(8, '1000000002-3', '58', 'none', 'Internal', 'GSAS', '2', '0', '2020-06-19 14:28:04'),
+(9, '1000000003-1', '58', 'none', 'External', 'asdasd', '2', '0', '2020-06-19 14:30:06'),
+(10, '1000000003-2', '58', 'none', 'External', 'asdasd', '2', '0', '2020-06-19 14:30:06'),
+(11, '1000000004-1', '58', 'none', 'Internal', 'NMPMNL', '2', '0', '2020-06-19 14:33:37'),
+(12, '1000000004-2', '58', 'none', 'Internal', 'MTS', '2', '0', '2020-06-19 14:33:37'),
+(13, '1000000005-1', '58', 'none', 'External', 'asdasd', '2', '0', '2020-06-19 14:34:48'),
+(14, '1000000005-2', '58', 'none', 'External', 'asdasd', '2', '0', '2020-06-19 14:34:48'),
+(15, '1000000006-1', '58', 'none', 'Internal', 'SMOS', '2', '0', '2020-06-19 14:36:18'),
+(16, '1000000006-2', '58', 'none', 'Internal', 'MTS', '2', '0', '2020-06-19 14:36:18'),
+(17, '1000000007-1', '58', 'none', 'Internal', 'Motor Pool', '2', '0', '2020-06-19 14:37:43'),
+(18, '1000000007-2', '58', 'none', 'Internal', 'DED', '2', '0', '2020-06-19 14:37:43'),
+(19, '1000000008-1', '58', 'none', 'External', 'asdasdasd', '2', '0', '2020-06-19 14:38:18'),
+(20, '1000000008-2', '58', 'none', 'External', 'asdasd', '2', '0', '2020-06-19 14:38:18'),
+(21, '1000000009-1', '58', 'none', 'Internal', 'HRMS', '2', '0', '2020-06-19 14:38:43'),
+(22, '1000000009-2', '58', 'none', 'Internal', 'MTS', '2', '0', '2020-06-19 14:38:43'),
+(23, '1000000010-1', '58', 'none', 'Internal', 'HRMS', '2', '0', '2020-06-19 14:39:19'),
+(24, '1000000010-2', '58', 'none', 'Internal', 'MAS', '2', '0', '2020-06-19 14:39:19'),
+(25, '1000000011-1', '58', 'none', 'External', 'asdasd', '2', '0', '2020-06-19 14:40:53'),
+(26, '1000000011-2', '58', 'none', 'External', 'asdasd', '2', '0', '2020-06-19 14:40:53'),
+(27, '1000000012-1', '58', 'none', 'Internal', 'MRMS', '2', '0', '2020-06-19 14:41:21'),
+(28, '1000000012-2', '58', 'none', 'Internal', 'Budget', '2', '0', '2020-06-19 14:41:21'),
+(29, '1000000013-1', '58', 'none', 'External', 'asdasd', '2', '0', '2020-06-19 14:41:42'),
+(30, '1000000013-2', '58', 'none', 'External', 'sdfsdf', '2', '0', '2020-06-19 14:41:42'),
+(31, '1000000014-1', '58', 'none', 'Internal', 'IMS', '2', '0', '2020-06-19 14:46:18'),
+(32, '1000000014-2', '58', 'none', 'Internal', 'Motor Pool', '2', '0', '2020-06-19 14:46:18');
+
 -- --------------------------------------------------------
 
 --
@@ -77,8 +115,62 @@ CREATE TABLE `documents` (
   `note` varchar(200) NOT NULL,
   `date_time_created` timestamp NOT NULL DEFAULT current_timestamp(),
   `status` varchar(11) NOT NULL,
-  `ref` varchar(50) NOT NULL
+  `ref` varchar(50) NOT NULL,
+  `category` varchar(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `documents`
+--
+
+INSERT INTO `documents` (`documentID`, `creator`, `subject`, `doc_type`, `note`, `date_time_created`, `status`, `ref`, `category`) VALUES
+('1000000000', '58', 'Laudantium ducimus', '7', 'Quisquam quasi conse', '2020-06-18 01:20:49', '1', '0', '2'),
+('1000000000-1', '58', 'Laudantium ducimus', '7', 'Quisquam quasi conse', '2020-06-18 01:20:49', '1', '1000000000', '2'),
+('1000000000-2', '58', 'Laudantium ducimus', '7', 'Quisquam quasi conse', '2020-06-18 01:20:49', '1', '1000000000', '2'),
+('1000000000-3', '58', 'Laudantium ducimus', '7', 'Quisquam quasi conse', '2020-06-18 01:20:49', '1', '1000000000', '2'),
+('1000000001', '58', 'Omnis tenetur et aut', '3', 'Laboriosam vitae pr', '2020-06-18 01:55:34', '1', '0', '1'),
+('1000000001-1', '58', 'Omnis tenetur et aut', '3', 'Laboriosam vitae pr', '2020-06-18 01:55:34', '1', '1000000001', '1'),
+('1000000001-2', '58', 'Omnis tenetur et aut', '3', 'Laboriosam vitae pr', '2020-06-18 01:55:34', '1', '1000000001', '1'),
+('1000000002', '58', 'Non sed sunt odio et', '16', 'Ipsum quas officia ', '2020-06-19 06:28:04', '1', '0', '2'),
+('1000000002-1', '58', 'Non sed sunt odio et', '16', 'Ipsum quas officia ', '2020-06-19 06:28:04', '1', '1000000002', '2'),
+('1000000002-2', '58', 'Non sed sunt odio et', '16', 'Ipsum quas officia ', '2020-06-19 06:28:04', '1', '1000000002', '2'),
+('1000000002-3', '58', 'Non sed sunt odio et', '16', 'Ipsum quas officia ', '2020-06-19 06:28:04', '1', '1000000002', '2'),
+('1000000003', '58', 'Quo vitae nemo praes', '12', 'Placeat hic volupta', '2020-06-19 06:30:06', '1', '0', '2'),
+('1000000003-1', '58', 'Quo vitae nemo praes', '12', 'Placeat hic volupta', '2020-06-19 06:30:06', '1', '1000000003', '2'),
+('1000000003-2', '58', 'Quo vitae nemo praes', '12', 'Placeat hic volupta', '2020-06-19 06:30:06', '1', '1000000003', '2'),
+('1000000004', '58', 'Doloremque eligendi ', '11', 'Assumenda culpa mole', '2020-06-19 06:33:37', '1', '0', '1'),
+('1000000004-1', '58', 'Doloremque eligendi ', '11', 'Assumenda culpa mole', '2020-06-19 06:33:37', '1', '1000000004', '1'),
+('1000000004-2', '58', 'Doloremque eligendi ', '11', 'Assumenda culpa mole', '2020-06-19 06:33:37', '1', '1000000004', '1'),
+('1000000005', '58', 'Vero in sint molliti', '3', 'In in nostrum eos qu', '2020-06-19 06:34:48', '1', '0', '1'),
+('1000000005-1', '58', 'Vero in sint molliti', '3', 'In in nostrum eos qu', '2020-06-19 06:34:48', '1', '1000000005', '1'),
+('1000000005-2', '58', 'Vero in sint molliti', '3', 'In in nostrum eos qu', '2020-06-19 06:34:48', '1', '1000000005', '1'),
+('1000000006', '58', 'Nostrum nihil eius o', '15', 'Deserunt cillum ulla', '2020-06-19 06:36:18', '1', '0', '1'),
+('1000000006-1', '58', 'Nostrum nihil eius o', '15', 'Deserunt cillum ulla', '2020-06-19 06:36:18', '1', '1000000006', '1'),
+('1000000006-2', '58', 'Nostrum nihil eius o', '15', 'Deserunt cillum ulla', '2020-06-19 06:36:18', '1', '1000000006', '1'),
+('1000000007', '58', 'Voluptatem Et exerc', '7', 'Harum culpa cumque ', '2020-06-19 06:37:43', '1', '0', '2'),
+('1000000007-1', '58', 'Voluptatem Et exerc', '7', 'Harum culpa cumque ', '2020-06-19 06:37:43', '1', '1000000007', '2'),
+('1000000007-2', '58', 'Voluptatem Et exerc', '7', 'Harum culpa cumque ', '2020-06-19 06:37:43', '1', '1000000007', '2'),
+('1000000008', '58', 'Voluptas consequuntu', '13', 'Est sed sunt culpa', '2020-06-19 06:38:18', '1', '0', '1'),
+('1000000008-1', '58', 'Voluptas consequuntu', '13', 'Est sed sunt culpa', '2020-06-19 06:38:18', '1', '1000000008', '1'),
+('1000000008-2', '58', 'Voluptas consequuntu', '13', 'Est sed sunt culpa', '2020-06-19 06:38:18', '1', '1000000008', '1'),
+('1000000009', '58', 'Ipsam sapiente commo', '5', 'Consequuntur aliquam', '2020-06-19 06:38:43', '1', '0', ''),
+('1000000009-1', '58', 'Ipsam sapiente commo', '5', 'Consequuntur aliquam', '2020-06-19 06:38:43', '1', '1000000009', ''),
+('1000000009-2', '58', 'Ipsam sapiente commo', '5', 'Consequuntur aliquam', '2020-06-19 06:38:43', '1', '1000000009', ''),
+('1000000010', '58', 'Dignissimos ex magni', '10', 'Et aut doloribus et ', '2020-06-19 06:39:19', '1', '0', '3'),
+('1000000010-1', '58', 'Dignissimos ex magni', '10', 'Et aut doloribus et ', '2020-06-19 06:39:19', '1', '1000000010', '3'),
+('1000000010-2', '58', 'Dignissimos ex magni', '10', 'Et aut doloribus et ', '2020-06-19 06:39:19', '1', '1000000010', '3'),
+('1000000011', '58', 'Aliqua Dolor aliqua', '14', 'Officia et in placea', '2020-06-19 06:40:53', '1', '0', '3'),
+('1000000011-1', '58', 'Aliqua Dolor aliqua', '14', 'Officia et in placea', '2020-06-19 06:40:53', '1', '1000000011', '3'),
+('1000000011-2', '58', 'Aliqua Dolor aliqua', '14', 'Officia et in placea', '2020-06-19 06:40:53', '1', '1000000011', '3'),
+('1000000012', '58', 'Unde ullamco cumque ', '5', 'Atque quia non rerum', '2020-06-19 06:41:21', '1', '0', '1'),
+('1000000012-1', '58', 'Unde ullamco cumque ', '5', 'Atque quia non rerum', '2020-06-19 06:41:21', '1', '1000000012', '1'),
+('1000000012-2', '58', 'Unde ullamco cumque ', '5', 'Atque quia non rerum', '2020-06-19 06:41:21', '1', '1000000012', '1'),
+('1000000013', '58', 'Molestiae amet ut d', '10', 'Saepe rerum officia ', '2020-06-19 06:41:42', '1', '0', '1'),
+('1000000013-1', '58', 'Molestiae amet ut d', '10', 'Saepe rerum officia ', '2020-06-19 06:41:42', '1', '1000000013', '1'),
+('1000000013-2', '58', 'Molestiae amet ut d', '10', 'Saepe rerum officia ', '2020-06-19 06:41:42', '1', '1000000013', '1'),
+('1000000014', '58', 'Aut itaque Nam illo ', '15', 'Ea ut aut aliquam qu', '2020-06-19 06:46:18', '1', '0', '3'),
+('1000000014-1', '58', 'Aut itaque Nam illo ', '15', 'Ea ut aut aliquam qu', '2020-06-19 06:46:18', '1', '1000000014', '3'),
+('1000000014-2', '58', 'Aut itaque Nam illo ', '15', 'Ea ut aut aliquam qu', '2020-06-19 06:46:18', '1', '1000000014', '3');
 
 -- --------------------------------------------------------
 
@@ -119,6 +211,63 @@ CREATE TABLE `document_action_req` (
   `action_req` varchar(120) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dumping data for table `document_action_req`
+--
+
+INSERT INTO `document_action_req` (`document_action_req_id`, `documentID`, `action_req`) VALUES
+(1, '1000000000', 'For Comment'),
+(2, '1000000000', 'For Information'),
+(3, '1000000001', 'For Approval'),
+(4, '1000000001', 'For Signature'),
+(5, '1000000001', 'For Recommendation'),
+(6, '1000000001', 'For Comment'),
+(7, '1000000001', 'For Information'),
+(8, '1000000001', 'For File'),
+(9, '1000000002', 'For Approval'),
+(10, '1000000002', 'For Endorsement'),
+(11, '1000000002', 'For Recommendation'),
+(12, '1000000002', 'For Comment'),
+(13, '1000000002', 'For File'),
+(14, '1000000003', 'For Approval'),
+(15, '1000000003', 'For Signature'),
+(16, '1000000003', 'For Endorsement'),
+(17, '1000000003', 'For Comment'),
+(18, '1000000003', 'For File'),
+(19, '1000000004', 'For Endorsement'),
+(20, '1000000004', 'For Recommendation'),
+(21, '1000000004', 'For Comment'),
+(22, '1000000004', 'For File'),
+(23, '1000000005', 'For Approval'),
+(24, '1000000005', 'For Endorsement'),
+(25, '1000000005', 'For Recommendation'),
+(26, '1000000005', 'For Action'),
+(27, '1000000006', 'For Signature'),
+(28, '1000000006', 'For Action'),
+(29, '1000000006', 'For Information'),
+(30, '1000000006', 'For File'),
+(31, '1000000007', 'For Approval'),
+(32, '1000000007', 'For Action'),
+(33, '1000000007', 'For Comment'),
+(34, '1000000007', 'For Information'),
+(35, '1000000008', 'For Approval'),
+(36, '1000000008', 'For Recommendation'),
+(37, '1000000008', 'For Action'),
+(38, '1000000010', 'For Approval'),
+(39, '1000000011', 'For Action'),
+(40, '1000000012', 'For Signature'),
+(41, '1000000012', 'For Endorsement'),
+(42, '1000000012', 'For Recommendation'),
+(43, '1000000013', 'For Signature'),
+(44, '1000000013', 'For Endorsement'),
+(45, '1000000013', 'For Recommendation'),
+(46, '1000000013', 'For Action'),
+(47, '1000000014', 'For Signature'),
+(48, '1000000014', 'For Endorsement'),
+(49, '1000000014', 'For Recommendation'),
+(50, '1000000014', 'For Action'),
+(51, '1000000014', 'For Comment');
+
 -- --------------------------------------------------------
 
 --
@@ -152,6 +301,27 @@ INSERT INTO `document_type` (`id`, `type`) VALUES
 (15, 'Requisition and Issue Voucher'),
 (16, 'Unclassified'),
 (18, 'Travel Documents');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `doc_category`
+--
+
+CREATE TABLE `doc_category` (
+  `id` int(11) NOT NULL,
+  `section_id` varchar(11) NOT NULL,
+  `category` varchar(120) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `doc_category`
+--
+
+INSERT INTO `doc_category` (`id`, `section_id`, `category`) VALUES
+(1, '1', 'HRMS'),
+(2, '1', 'RIS'),
+(3, '1', 'Planning');
 
 -- --------------------------------------------------------
 
@@ -296,12 +466,12 @@ INSERT INTO `users_session` (`id`, `userId`, `timeStamp`, `isDeleted`) VALUES
 (2, '49', '2020-06-09 05:02:02', '1'),
 (3, '50', '2020-06-01 07:37:56', '1'),
 (4, '51', '2020-06-08 06:55:46', '1'),
-(5, '52', '2020-06-16 02:50:34', '1'),
+(5, '52', '2020-06-19 05:07:02', '1'),
 (6, '53', '2020-06-01 07:38:03', '1'),
 (7, '57', '2020-05-29 02:45:30', '1'),
-(8, '58', '2020-06-09 05:02:13', '0'),
-(9, '59', '2020-06-09 02:35:43', '1'),
-(10, '60', '2020-06-02 05:32:56', '1'),
+(8, '58', '2020-06-19 05:07:12', '0'),
+(9, '59', '2020-06-18 06:34:24', '1'),
+(10, '60', '2020-06-18 06:34:28', '1'),
 (11, '61', '2020-06-08 06:18:26', '1');
 
 -- --------------------------------------------------------
@@ -365,6 +535,12 @@ ALTER TABLE `document_type`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `doc_category`
+--
+ALTER TABLE `doc_category`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `sections`
 --
 ALTER TABLE `sections`
@@ -416,7 +592,7 @@ ALTER TABLE `divisions`
 -- AUTO_INCREMENT for table `documentLogs`
 --
 ALTER TABLE `documentLogs`
-  MODIFY `trans_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `trans_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=33;
 
 --
 -- AUTO_INCREMENT for table `documentStatus`
@@ -428,13 +604,19 @@ ALTER TABLE `documentStatus`
 -- AUTO_INCREMENT for table `document_action_req`
 --
 ALTER TABLE `document_action_req`
-  MODIFY `document_action_req_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `document_action_req_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=52;
 
 --
 -- AUTO_INCREMENT for table `document_type`
 --
 ALTER TABLE `document_type`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+
+--
+-- AUTO_INCREMENT for table `doc_category`
+--
+ALTER TABLE `doc_category`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `sections`
