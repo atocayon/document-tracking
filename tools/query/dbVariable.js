20279310e0d8ea4d90ef4114bb22800c4166cf78
Aljon Tocayon <actwhiteHat27@gmail.com>
2020-08-12 09:03:16 +0800

    temp: local configurations

--- tools/query/dbVariable.js
+++ tools/query/dbVariable.js
@@ -1,5 +1,5 @@
-exports.user = "root";
-exports.password = "";
-exports.database = "documentTracking";
+exports.user = "aljon";
+exports.password = "@AljonTocayon1427";
+exports.database = "documenttracking";
 exports.host = "localhost";
 exports.port = "3306";
